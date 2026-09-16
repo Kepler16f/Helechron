83069967614256f3f6af390fe91a5910bb20b8b6
ci <example@example.com>
2026-09-16 18:33:05 +0800

    v0.0.4_dev2: fix widget data display by adding LocalStorage to @Entry decorator

--- lib/worker/fuse.dart
+++ lib/worker/fuse.dart
@@ -8,8 +8,8 @@ class Fuse {
   late DateTime lastUpdateTime;
 
   final bool isBeta = true;
-  final version = [0, 0, 3];
-  final build = 15;
+  final version = [0, 0, 4];
+  final build = 16;
   List<int>? remoteVersion;
   int? remoteBuild;
   bool hasNewVersion = false;
@@ -17,7 +17,7 @@ class Fuse {
   final HttpClient _httpClient = HttpClient();
   final DatabaseHelper _db = Get.find<DatabaseHelper>(tag: 'db');
 
-  String get displayVersion => '0.0.4_dev1';
+  String get displayVersion => '0.0.4_dev2';
 
   Fuse() {
     lastUpdateTime = DateTime(2001, 1, 1);
