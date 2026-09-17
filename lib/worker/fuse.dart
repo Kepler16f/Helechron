be9574a286cafd6d676b9bacddcab28aa4147255
ci <example@example.com>
2026-09-18 00:57:56 +0800

    feat(widget): 课程简报改2*2+日期 | 刷新按钮样式统一 | 修复倒计时刷新按钮 (v0.0.4_dev9)

--- lib/worker/fuse.dart
+++ lib/worker/fuse.dart
@@ -9,7 +9,7 @@ class Fuse {
 
   final bool isBeta = true;
   final version = [0, 0, 4];
-  final build = 30;
+  final build = 31;
   List<int>? remoteVersion;
   int? remoteBuild;
   bool hasNewVersion = false;
@@ -17,7 +17,7 @@ class Fuse {
   final HttpClient _httpClient = HttpClient();
   final DatabaseHelper _db = Get.find<DatabaseHelper>(tag: 'db');
 
-  String get displayVersion => '0.0.4_dev8';
+  String get displayVersion => '0.0.4_dev9';
 
   Fuse() {
     lastUpdateTime = DateTime(2001, 1, 1);
