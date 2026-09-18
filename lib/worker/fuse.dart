d7e8b67f822291ac5f31f7157fca1422a436b394
ci <example@example.com>
2026-09-18 14:19:56 +0800

    fix(widget): 课程简报显示教师 | 版本0.0.4正式发布

--- lib/worker/fuse.dart
+++ lib/worker/fuse.dart
@@ -9,7 +9,7 @@ class Fuse {
 
   final bool isBeta = true;
   final version = [0, 0, 4];
-  final build = 32;
+  final build = 33;
   List<int>? remoteVersion;
   int? remoteBuild;
   bool hasNewVersion = false;
@@ -17,7 +17,7 @@ class Fuse {
   final HttpClient _httpClient = HttpClient();
   final DatabaseHelper _db = Get.find<DatabaseHelper>(tag: 'db');
 
-  String get displayVersion => '0.0.4_dev10';
+  String get displayVersion => '0.0.4';
 
   Fuse() {
     lastUpdateTime = DateTime(2001, 1, 1);
