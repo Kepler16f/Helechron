a38f72cc44087594a32723a56ce000e0fbe9ce14
ci <example@example.com>
2026-09-20 14:41:04 +0800

    fix: 系统沉浸光感材质生效修复 - 配置targetSdkVersion 26.0.0 + module级UIMaterial.state=enable + 材质探测降级 + 关闭默认渐变蒙层 (v0.1.0_dev13)

--- lib/worker/fuse.dart
+++ lib/worker/fuse.dart
@@ -9,7 +9,7 @@ class Fuse {
 
   final bool isBeta = true;
   final version = [0, 0, 4];
-  final build = 45;
+  final build = 46;
   List<int>? remoteVersion;
   int? remoteBuild;
   bool hasNewVersion = false;
@@ -17,7 +17,7 @@ class Fuse {
   final HttpClient _httpClient = HttpClient();
   final DatabaseHelper _db = Get.find<DatabaseHelper>(tag: 'db');
 
-  String get displayVersion => '0.1.0_dev12';
+  String get displayVersion => '0.1.0_dev13';
 
   Fuse() {
     lastUpdateTime = DateTime(2001, 1, 1);
