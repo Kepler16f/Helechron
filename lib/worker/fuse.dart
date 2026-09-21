63b657fa45b22a372fd6234146414808e29b53c0
ci <example@example.com>
2026-09-21 23:50:27 +0800

    chore: bump to v0.1.1_dev3 (build 51)

--- lib/worker/fuse.dart
+++ lib/worker/fuse.dart
@@ -9,7 +9,7 @@ class Fuse {
 
   final bool isBeta = true;
   final version = [0, 1, 1];
-  final build = 50;
+  final build = 51;
   List<int>? remoteVersion;
   int? remoteBuild;
   bool hasNewVersion = false;
@@ -17,7 +17,7 @@ class Fuse {
   final HttpClient _httpClient = HttpClient();
   final DatabaseHelper _db = Get.find<DatabaseHelper>(tag: 'db');
 
-  String get displayVersion => '0.1.1_dev2';
+  String get displayVersion => '0.1.1_dev3';
 
   Fuse() {
     lastUpdateTime = DateTime(2001, 1, 1);
