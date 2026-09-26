fb1c3178e0ab3ab5a9e4cd311f13f68e9453922a
ci <example@example.com>
2026-09-26 20:25:34 +0800

    fix: 修复付款码放大页刷新按钮深色模式适配与课程小组件模糊刷新文案 (v0.1.2_dev5)

--- lib/worker/fuse.dart
+++ lib/worker/fuse.dart
@@ -9,7 +9,7 @@ class Fuse {
 
   final bool isBeta = true;
   final version = [0, 1, 2];
-  final build = 61;
+  final build = 62;
   List<int>? remoteVersion;
   int? remoteBuild;
   bool hasNewVersion = false;
@@ -17,7 +17,7 @@ class Fuse {
   final HttpClient _httpClient = HttpClient();
   final DatabaseHelper _db = Get.find<DatabaseHelper>(tag: 'db');
 
-  String get displayVersion => '0.1.2_dev4';
+  String get displayVersion => '0.1.2_dev5';
 
   Fuse() {
     lastUpdateTime = DateTime(2001, 1, 1);
